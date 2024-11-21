4edb273c7398b1b6c2195395c47323692b392474
KChronos000 <damnwork186@gmail.com>
2024-11-21 12:19:43 +0700

    init

--- app/(backend)/admin/education/terms/page.tsx
+++ app/(backend)/admin/education/terms/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getEducationTerms } from '@/app/functions/settings/educations/Educations'
 import { EducationTerm } from '@/app/types/Settings/Educations'
 import { EducationTermAdd, ListEducationTerms, SearchEducationTerm } from '@/components/Settings/EducationTerm'
@@ -29,33 +28,3 @@ export default async function ManageEducationTerm({searchParams}:{
         </div>
     )
 }
-=======
-import { getEducationTerms } from '@/app/functions/settings/educations/Educations'
-import { EducationTerm } from '@/app/types/Settings/Educations'
-import { EducationTermAdd, ListEducationTerms, SearchEducationTerm } from '@/components/Settings/EducationTerm'
-import React from 'react'
-
-import type { Metadata } from 'next'
- 
-export const metadata: Metadata = {
-  title: 'จัดการภาคเรียน | โรงเรียนภูเขียว',
-  description: 'จัดการภาคเรียน โรงเรียนภูเขียว',
-}
-
-export default async function ManageEducationTerm({searchParams}:{
-    searchParams: { [key: string]: string | string[] | undefined }
-}) {
-    const search = typeof searchParams.search === 'string' ? searchParams.search : undefined;    
-    const page = typeof searchParams.page === 'string' ? Number(searchParams.page) : undefined;    
-    const terms : EducationTerm = await getEducationTerms({search:search,page:page})
-    return (
-        <div className="p-4 space-y-4">
-            <h1 className="text-2xl font-bold">จัดการภาคเรียน</h1>
-
-            <EducationTermAdd />
-            <SearchEducationTerm />
-            <ListEducationTerms terms={terms} />
-        </div>
-    )
-}
->>>>>>> c79b52ff77a62793c36205fd4eee323eb1adfd69
--- app/(backend)/admin/students/page.tsx
+++ app/(backend)/admin/students/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getStudents } from '@/app/functions/settings/students/Students';
 import { Student } from '@/app/types/Settings/Students';
 import { ListStudents, SearchStudent, StudentAdd } from '@/components/Settings/Student';
@@ -28,32 +27,3 @@ export default async function ManageStudent({searchParams}:{
         </div>
     )
 }
-=======
-import { getStudents } from '@/app/functions/settings/students/Students';
-import { Student } from '@/app/types/Settings/Students';
-import { ListStudents, SearchStudent, StudentAdd } from '@/components/Settings/Student';
-import React from 'react'
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-    title: 'จัดการนักเรียน | โรงเรียนภูเขียว',
-    description: 'จัดการนักเรียน โรงเรียนภูเขียว',
-}
-
-export default async function ManageStudent({searchParams}:{
-    searchParams: { [key: string]: string | string[] | undefined }
-}) {
-    const search = typeof searchParams.search === 'string' ? searchParams.search : undefined;    
-    const page = typeof searchParams.page === 'string' ? Number(searchParams.page) : undefined;    
-    const students : Student = await getStudents({search:search,page:page})
-    return (
-        <div className="p-4 space-y-4">
-            <h1 className="text-2xl font-bold">จัดการนักเรียน</h1>
-
-            <StudentAdd />
-            <SearchStudent />
-            <ListStudents students={students} />
-        </div>
-    )
-}
->>>>>>> c79b52ff77a62793c36205fd4eee323eb1adfd69
