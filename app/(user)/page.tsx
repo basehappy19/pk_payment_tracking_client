c8b2f90d22b32f306ac6e4c4d92590e467f2fb09
basehappy19 <pakpoom41233teedindam@gmal.com>
2024-11-23 14:20:51 +0700

    init

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { Metadata } from 'next'
 import MainMenu from "@/components/MainMenu";
 import { getUserData } from '../functions/getUserData';
@@ -17,23 +16,3 @@ export default async function Home() {
     </main>
   );
 }
-=======
-import type { Metadata } from 'next'
-import MainMenu from "@/components/MainMenu";
-import { getUserData } from '../functions/getUserData';
-
-export const metadata: Metadata = {
-  title: 'ระบบตรวจสอบค่าบำรุงการศึกษา | โรงเรียนภูเขียว',
-  description: 'ระบบตรวจสอบค่าบำรุงการศึกษา โรงเรียนภูเขียว',
-}
-
-export default async function Home() {
-  const session = await getUserData()
-  
-  return (
-    <main className="bg-gradient-to-t from-pink-300/70 to-fuchsia-200/50 dark:to-fuchsia-800/5 dark:from-red-700/15 flex items-center justify-center min-h-screen p-4">
-      <MainMenu session={session} />
-    </main>
-  );
-}
->>>>>>> 4edb273c7398b1b6c2195395c47323692b392474
--- components/auth/CardLoginStudent.tsx
+++ components/auth/CardLoginStudent.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
@@ -51,58 +50,4 @@ export default function CardLoginStudent() {
             </Card>
         </div>
     )
-=======
-'use client'
-import { signIn } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { FormEvent } from 'react'
-import { toast } from 'react-toastify'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
-import AfterSignIn from '@/app/action/Auth/AfterSignIn'
-
-export default function CardLoginStudent() {
-    const router = useRouter()
-
-    async function onSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-        const formData = new FormData(e.currentTarget)
-        const res = await signIn("student-login", {
-            sid: formData.get('sid') as string,
-            cid: formData.get('cid') as string,
-            redirect: false,
-        })
-        await AfterSignIn();
-        if (res?.error) {
-            return toast.error('รหัสนักเรียน หรือ เลขบัตรประชาชนไม่ถูกต้อง', { position: 'bottom-right'});
-        }
-        router.push('/fee');
-    }
-
-    return (
-        <main className="flex justify-center items-center min-h-[80vh]">
-            <Card className="w-full md:w-1/2 bg-gradient-to-t from-pink-200 to-red-200 border-pink-400 border-2 dark:from-stone-800 dark:to-zinc-800 dark:border-stone-700/50">
-                <CardHeader>
-                    <CardTitle className="text-2xl font-medium text-center">ตรวจค่าบำรุงการศึกษานักเรียน</CardTitle>
-                    <CardDescription className="text-center text-pink-500">กรุณากรอกข้อมูลเพื่อเข้าสู่ระบบ</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form onSubmit={onSubmit} className="space-y-4">
-                        <div className="space-y-2">
-                            <Label htmlFor="sid">รหัสนักเรียน</Label>
-                            <Input className='border-pink-300 border dark:border-stone-800' id="sid" name="sid" type="text" placeholder="กรอกรหัสนักเรียน" required />
-                        </div>
-                        <div className="space-y-2">
-                            <Label htmlFor="cid">เลขบัตรประชาชน</Label>
-                            <Input className='border-pink-300 border dark:border-stone-800' id="cid" name="cid" type="password" placeholder="กรอกเลขบัตรประชาชน" required />
-                        </div>
-                        <Button type="submit" className="w-full">เข้าสู่ระบบ</Button>
-                    </form>
-                </CardContent>
-            </Card>
-        </main>
-    )
->>>>>>> 4edb273c7398b1b6c2195395c47323692b392474
 }
